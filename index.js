4c265d3c8130d32284e7f58cc79b8a40377d77fb
sid-4998 <siddhantjha34@gmail.com>
2026-01-19 20:53:09 +0530

    CLI setup

---
+++ index.js
@@ -0,0 +1,48 @@
+import readline from 'readline';
+import chalk from 'chalk';
+
+console.log("=".repeat(20) + chalk.blue(' Welcome to Task Tracker CLI ') + "=".repeat(20));
+console.log("*".repeat(3) + (' Instructions') + ":");
+console.log("#" + ' Adding a task');
+console.log("#" + chalk.yellow(' add ') + '"task"');
+console.log("#" + ' Deleting tasks');
+console.log("#" + chalk.red(' delete ') + chalk.blue('id1, id2, id3.....'));
+console.log("#" + ' Deleting all tasks');
+console.log("#" + chalk.red(' delete .'));
+console.log("#" + ' Updating a task');
+console.log("#" + chalk.yellowBright(' Update ') + chalk.blue('Id') + ' "new task"');
+console.log("#" + ' Marking tasks done');
+console.log("#" + chalk.green(' mark-done ') + chalk.blue('id1, id2, id3.....'));
+console.log("#" + ' Marking tasks in progress');
+console.log("#" + chalk.magenta(' mark-in-progress ') + chalk.blue('id1, id2, id3.....'));
+console.log("#" + ' Marking all tasks done');
+console.log("#" + chalk.green(' mark-done .'))
+console.log("#" + ' Marking all tasks in progress');
+console.log("#" + chalk.magenta(' mark-in-progress .'));
+console.log("#" + ' Listing all tasks');
+console.log("#" + chalk.gray(' list .'));
+console.log("#" + ' Listing all pending tasks');
+console.log("#" + chalk.gray(' list todo'));
+console.log("#" + ' Listing all complete tasks');
+console.log("#" + chalk.gray(' list done'));
+console.log("#" + ' Listing all tasks in progress');
+console.log("#" + chalk.gray(' list in-progress'));
+ 
+const taskCLI = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'task> ',
+});
+
+taskCLI.prompt();
+
+taskCLI.on('line', (line) => {
+    const data = line.trim();
+    console.log(data);
+    taskCLI.prompt();
+});
+
+taskCLI.on('close', () => {
+  console.log('Goodbye!');
+  process.exit(0);
+});
